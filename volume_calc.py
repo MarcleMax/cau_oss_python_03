b5a98cb4b02725cbd0c0ffb6f8c2d059a445f807
MarcleMax <127838206+MarcleMax@users.noreply.github.com>
2023-03-14 17:55:21 +0800

    Create volume_calc.py
    기본 버전

---
+++ volume_calc.py
@@ -0,0 +1,7 @@
+lenth = float(input('가로를 입력해 주십시오: '))
+width = float(input('세로를 입력해 주십시오: '))
+height = float(input('높이를 입력해 주십시오: '))
+
+volume = lenth * width * height
+
+print('박스의 부피는%s입니다.' %volume)
