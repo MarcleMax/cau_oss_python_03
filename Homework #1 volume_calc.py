0f75fffcde405d568f229677be55b7f88291b303
MarcleMax <127838206+MarcleMax@users.noreply.github.com>
2023-03-23 15:28:20 +0800

    Update Homework #1 volume_calc.py

--- Homework #1 volume_calc.py
+++ Homework #1 volume_calc.py
@@ -10,11 +10,11 @@ express_fee = 0      # initialize the variable in advance for easier code changi
 total_length = length + width + height
 if (0 < total_length <= 80):        # the box's total length shoud greater than 0
     express_fee = 5000
-elif (80 < total_length <= 100):
+elif (total_length <= 100):
     express_fee = 8000
-elif (100 < total_length <= 120):
+elif (total_length <= 120):
     express_fee = 10000
-elif (120 < total_length <= 160):
+elif (total_length <= 160):
     express_fee = 13000
 else:
     print("Sorry, please ask the staff for the details of express fee.")
