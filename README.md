baa585863e40f6eb4c9bf56eab551b1c2d8ce195
MarcleMax <virzyr7@gmail.com>
2023-04-11 02:03:14 +0900

    아, 힘들다
    자고 싶다ㅍㅍ

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cau_oss_python_03
 중앙대학교 오픈소SW와 파이썬 프로그래밍 3분반 장육예입니다.
+
+
+## 2023-04-10
+Homework#2, simple calculator 구현
---
+++ simple_calculator.py
@@ -0,0 +1,28 @@
+﻿def add(x, y):	return(x, y, x + y)
+def sub(x, y):	return(x, y, x + y)
+def arithmetic_ops(op):
+    num1 = int(input("input 1st number:"))
+    num2 = int(input("input 2nd number:"))
+    return num1, num2, op(num1, num2)
+
+
+while True:
+    op = input("input operation:")
+    if op == "end":
+        break   # "반복을 종료"
+    elif op == "+":
+        num1, num2, ret = arithmetic_ops(add) # 정의된 함수 사용
+    elif op == "-":
+        num1, num2, ret = arithmetic_ops(sub)
+    elif op == "*":
+        num1, num2, ret = arithmetic_ops(lambda x, y: x * y) # 익명함수(lambda) 사용
+    elif op == "/":
+        num1, num2, ret = arithmetic_ops(lambda x, y: x / y)
+    elif op == "%":
+        num1, num2, ret = arithmetic_ops(lambda x, y: x % y)
+    else:
+        print("Invalid operation")
+        continue # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
+    print(f"{num1}{op}{num2} = {ret}")  # 연산 결과를 출력
+
+print("Exit program")
