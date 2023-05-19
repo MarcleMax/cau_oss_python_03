69e2464f16f2cec5cbfc9f0474d0c22310b2c1e5
markle <virzyr7@gmail.com>
2023-05-19 19:59:22 +0900

    supplementary upload.

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # cau_oss_python_03
 중앙대학교 오픈소SW와 파이썬 프로그래밍 3분반 장육예입니다.
 
-
 ## 2023-04-10
 Homework#2, simple calculator 구현
+
+## 2023-05-02
+Diff 
+
+## 2023-05-12
+Girlines
+
+## 2023-05-19
+Homework#4, figure module 
--- figure.py
+++ figure.py
@@ -1,28 +1,31 @@
 import math
 
 class line:
-# class 'line' is to generate the value of a line, preparing for the next operations
-# as a class, it's better to be named as 'Line' but we just ignore it here
+	'''
+	1. Class 'line' is a module for generating and accessing the length of a line, preparing for the next operations.
+	2. It has two functions which are 'set_length' and 'get_length'.
+	3. As a class, it's better to be named as 'Line' but we just ignore it here.
+	'''
 	def __init__(self, length=0):
-	# the initial value of the line is zero
+		''' '__init__' is for initalizing the value of line, while the value is '0'. '''
 		self.__length = length
 
 	def set_length(self, length):
-	# set_length is to set the value of the line
+		''' 'set_length' is to set the value of the line. '''
 		self.__length = length
 
 	def get_length(self):
-	# get_length is to get the value of the line for calculating later
+		''' 'get_length' is to get the value of the line for calculating later. '''
 		return self.__length
 
 def area_square(length):
-# area_square is to calculate: the area of a square, formed by the line we input as sides
+	''' 'area_square' is to calculate: the area of a square, formed by the line we input as sides. '''
 	return length ** 2
 
 def area_circle(length):
-# area_circle is to calculate: the area of a circle, formed by the line we input as radius
+	''' 'area_circle' is to calculate: the area of a circle, formed by the line we input as radius. '''
 	return length ** 2 * math.pi
 
 def area_regular_triangle(length):
-# area_regular_triangle is to calculate: the area of a regular triangle, formed by the line we input as sides
+	''' 'area_regular_triangle' is to calculate: the area of a regular triangle, formed by the line we input as sides. '''
 	return length ** 2 * (math.sqrt(3) / 4)
